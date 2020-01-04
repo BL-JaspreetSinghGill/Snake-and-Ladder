e6f5c703cb6925a4f527a4d7da11f80746646429
JaspreetSinghGill <jaspreetsg06@gmail.com>
2020-01-04 16:47:11 +0530

    ADDED: player checks for a option no play, ladder or snake

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -2,15 +2,41 @@
 
 START=0;
 GOAL=100;
-player1=0;
+player1=$START;
 
 rollDie () {
 	echo $(( RANDOM%6+1 ))
 }
 
+getOption () {
+	echo $(( RANDOM%3 ));
+}
+
+optionCheck () {
+	dieValue=$1;
+	result=$(getOption);
+
+	case $result in
+			0)
+				echo "NO PLAY";
+				;;
+			1)
+				echo "LADDER";
+				player1=$(( $player1+$dieValue ));
+				;;
+			2)
+				echo "SNAKE";
+				player1=$(( $player1-$dieValue ));
+				;;
+	esac;
+}
+
 snakeAndLadderMain () {
 	echo $player1;
-	rollDie;
+	dieValue=$(rollDie);
+	optionCheck $dieValue;
+
+	echo "PLAYER 1 : " $player1;
 }
 
 snakeAndLadderMain;
